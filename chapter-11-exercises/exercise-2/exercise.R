0086a6cc2af2200f653b9e642db55663675a0ea2
Alex Friedberg <blueal@users.noreply.github.com>
2020-05-26 07:01:42 -0700

    Finished Part 2

--- chapter-11-exercises/exercise-2/exercise.R
+++ chapter-11-exercises/exercise-2/exercise.R
@@ -8,41 +8,49 @@
 library(fueleconomy)
 
 # Install and load the "dplyr" library
-
+library(dplyr)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes = select(vehicles, make) %>% as.vector()
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+distinctMakes = makes %>%
+  distinct()
 
 # Filter the data set for vehicles manufactured in 1997
-
+Manufactured1997 = filter(vehicles, year == 1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
-
+Manufactured1997 = arrange(Manufactured1997, desc(hwy))
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+Manufactured1997 = mutate(Manufactured1997, average = (hwy + cty)/2)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+wheeldrive20mpg = filter(Manufactured1997, (drive != "4-Wheel or All-Wheel Drive")
+                         & (cty > 20))
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+worstVehicle = filter(wheeldrive20mpg, hwy == min(hwy)) %>% select(id)
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters,
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
+bestMpg = function(year_choice, make_choice){
+  bestVehicle = filter(vehicles, year == year_choice, make == make_choice,) %>%
+    filter(hwy == max(hwy)) %>%
+    select(model) %>%
+    toString()
+  return(bestVehicle)
+}
 
 # What was the most efficient Honda model of 1995?
-
+bestHonda = bestMpg(1995, "Honda")
